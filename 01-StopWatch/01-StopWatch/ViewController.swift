62797c375d0d4b01fd540add4e2401c7c75b0ed3
Guoyu <leeguoyu@Guoyu-MBP.local>
2016-03-03 22:34:53 +0800

    Finished Simple Watch
     Case temporary closed. Will see how to save local data later on.

--- 01-StopWatch/01-StopWatch/ViewController.swift
+++ 01-StopWatch/01-StopWatch/ViewController.swift
@@ -14,13 +14,49 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
     
-    var counter = 1.0
+    var counter = 0.0
+    var timer = NSTimer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        pauseButton.enabled = false
+        resetButton.enabled = false
         timeLabel.text = String(counter)
         
+    }
+    
+    //点击play按钮
+    @IBAction func playButtonDidClick(sender: AnyObject){
+        
+        playButton.enabled = false
+        pauseButton.enabled = true
+        resetButton.enabled = true
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("updateTimer"), userInfo: nil, repeats: true)
+        
+    }
+    
+    func updateTimer(){
+        
+        counter += 0.1
+        //这里一定要限制counter的小数位，不然会变超长
+        timeLabel.text = String(format:"%.1f", counter)
+        
+    }
+    //点击pause按钮
+    @IBAction func pauseButtonDidClick(sender: AnyObject){
+        playButton.enabled = true
+        pauseButton.enabled = false
+        timer.invalidate()
+    }
+    //点击reset按钮
+    @IBAction func resetButtonDidClick(sender: AnyObject){
+        timer.invalidate()
+        counter = 0.0
+        timeLabel.text = String(counter)
+        playButton.enabled = true
+        pauseButton.enabled = false
     }
 
 
