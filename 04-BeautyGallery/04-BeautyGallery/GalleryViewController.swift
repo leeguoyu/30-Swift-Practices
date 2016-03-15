9e896f2b9d014ead48182853717841c9f4ec491a
Guoyu <leeguoyu@Guoyu-MBP.local>
2016-03-15 21:30:45 +0800

    Added NavtigationController & Twitter share button

--- 04-BeautyGallery/04-BeautyGallery/GalleryViewController.swift
+++ 04-BeautyGallery/04-BeautyGallery/GalleryViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Social
 
 class GalleryViewController: UIViewController {
     
@@ -17,11 +18,39 @@ class GalleryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if imageName != nil{
+        if let name = imageName{
             
-            beautyImage.image = UIImage(named: imageName!)
+            beautyImage.image = UIImage(named: name)
+            
+            switch name{
+            case "fanbingbing":
+                navigationItem.title = "范冰冰"
+                
+            case "libingbing":
+                navigationItem.title = "李冰冰"
+
+            case "wangfei":
+                navigationItem.title = "王菲"
+
+            case "yangmi":
+                navigationItem.title = "杨幂"
+
+            case "zhouxun":
+                navigationItem.title = "周迅"
+                
+            default:
+                navigationItem.title = "女神"
+
+            }
         }
         
     }
     
+    @IBAction func shareButtonDidClick(sender: AnyObject) {
+        
+        let controller : SLComposeViewController = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+        controller.setInitialText("玩女神相册，不是玩女神")
+        controller.addImage(beautyImage.image)
+        self.presentViewController(controller, animated: true, completion: nil)
+    }
 }
